e641c4a7b8ce634cf55d8bde3e33f3c4e7537315
sebastiansoto1407 <sebastiansotoastete1407@gmail.com>
2025-03-13 10:47:06 -0400

    doom avance

---
+++ dom/script.js
@@ -0,0 +1,20 @@
+const btn = document.querySelector('[data-form-btn]')
+
+console.log(btn);
+
+const createTask =(evento)=>{
+    evento.preventDefault();
+    const input = document.querySelector('[data-form-input]');
+    console.log(input.value);
+    //funcion para recuperar el texto de mi input
+
+    const value= imput.value;
+    const list= document.querySelector('[data-list]')
+    const task= document.createElement('li')
+    task.classList.add('card');
+    input.value='';
+    
+}
+
+btn.addEventListener('click', createTask)
+
